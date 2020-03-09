3cb2e99463fa19ae15ec21de0b5438ffb6bc0acd
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2020-03-09 16:24:48 -0400

    another commit

---
+++ src/main/java/Biggie.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class Biggie {
+    public static void main(String[] args) {
+        System.out.println("The greatest rapper of all time died on" + new Date());
+    }
+}
